c395bd587b10d2b34d2115aee567b6bc340ee9f1
udaykiran1192 <160111537+udaykiran1192@users.noreply.github.com>
2024-08-03 11:51:18 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+
+//console.table()
+
+// variable declaration using const
+
+const accountId =1928;
+
+//variable declaration using let and var
+
+let accountMail = 'udaykiran@gmail.com';
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+// accountId=342; const variables can't be redecalred
+
+accountMail = "captainkiran420@gmail.com";
+
+accountPassword = "54321";
+accountCity = "Hyderabad";
+let accountState;
+console.log(accountId);
+console.table([accountState,accountMail,accountId,accountPassword,accountCity]);
+
+/**
+   To Decleare variabels in javascript use const or let
+   var is old
+   let is new
+   let is brought bacause of difference in block and functinal scope
+ */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+// "use strict"; //treat all js code as newer version of js
+
+/*
+  Data Types in java
+  1.Number
+  2.Float
+  3.String
+  4.Undefied
+  5.Null
+  6.Bollean
+
+*/
+
+// alert(3+3) using alert in this environment will not work
+// becuse ur using node js not browser
+// console.log("Hello World") same for Browser
+// "use strict";
+let name1 ="UdayKiran";
+let age = 1+2-"33";
+let isloggedIn=false;
+let amount;
+let money=Number(amount);
+
+let sym = Symbol('foo');
+console.table([name1,age,sym,isloggedIn,amount,money])
